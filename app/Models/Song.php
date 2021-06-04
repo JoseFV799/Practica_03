73c946427d0a9853f5cdd45da1e7a72d7676ba04
JoseFV799 <josecar712999@gmail.com>
2021-06-04 18:27:38 +0000

    Practica 03

---
+++ app/Models/Song.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Song extends Model
+{
+    use HasFactory;
+
+    protected $fillable = array(
+        'title', 'artist_group', 'release_date', 'track', 'cover',
+    );
+
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
+}
